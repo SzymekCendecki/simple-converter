f453efb93f91adbcc824c55e0c6bd5679880423c
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-05-25 21:14:13 +0200

    create simply coverter start

---
+++ js/engine.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+console.log("NIEWIERNE PSY RULEZ!!!!");
+
+$("#convert").on("click", ()=>{
+	let widthPercent = $("#widthOnPercent").val();
+	let onePercent = 13.66;
+	$("#score").text(onePercent*widthPercent + "px");
+});
+
+$("#convert2").on("click", ()=>{
+	let percent = $("#percentOnWidth").val();
+	
+	let onePercent = 13.66;
+	$("#score2").text(percent/onePercent + "%");
+});
+
+
+});
