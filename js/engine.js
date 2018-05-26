e36981654be34ea4e67b227a50a6d4e82a8acfb8
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-05-26 19:45:48 +0200

    change files structures, add list with resolutions

--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,100 @@
-document.addEventListener("DOMContentLoaded", () => {
-console.log("NIEWIERNE PSY RULEZ!!!!");
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-$("#convert").on("click", ()=>{
-	let widthPercent = $("#widthOnPercent").val();
-	let onePercent = 13.66;
-	$("#score").text(onePercent*widthPercent + "px");
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("NIEWIERNE PSY RULEZ!!!!");
+
+	$("#convert").on("click", function () {
+		var widthPercent = $("#widthOnPercent").val();
+		var onePercent = 13.66;
+		$("#score").text(onePercent * widthPercent + "px");
+	});
+
+	$("#convert2").on("click", function () {
+		var percent = $("#percentOnWidth").val();
+
+		var onePercent = 13.66;
+		$("#score2").text(percent / onePercent + "%");
+	});
+
+	$("#fifth").on("click", function () {});
 });
 
-$("#convert2").on("click", ()=>{
-	let percent = $("#percentOnWidth").val();
-	
-	let onePercent = 13.66;
-	$("#score2").text(percent/onePercent + "%");
-});
-
-
-});
+/***/ })
+/******/ ]);
