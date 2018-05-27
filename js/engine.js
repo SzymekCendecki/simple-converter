067a5ec3542122a5a1d014262cf96d53108b7376
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-05-27 16:51:28 +0200

    small code reorganization

--- js/engine.js
+++ js/engine.js
@@ -80,20 +80,77 @@ module.exports = __webpack_require__(1);
 document.addEventListener("DOMContentLoaded", function () {
 	console.log("NIEWIERNE PSY RULEZ!!!!");
 
-	$("#convert").on("click", function () {
-		var widthPercent = $("#widthOnPercent").val();
-		var onePercent = 13.66;
-		$("#score").text(onePercent * widthPercent + "px");
+	//obsługa zdarzeń dla przycisku dla rozdzielczości 1366x768 pikseli
+
+	$("#fifth").on("click", function () {
+
+		var title = document.createElement("p");
+		title.id = "title";
+		title.innerText = "Przeliczanie szerokości i wysokości z pikseli na procenty, dla rozdzielczości: 1366x768";
+		$(".main").append(title);
+
+		var descriptionWidthTitlePixels = document.createElement("p");
+		descriptionWidthTitlePixels.id = "descriptionWidthTitlePixels";
+		descriptionWidthTitlePixels.innerText = "podaj liczbę (w px)";
+		$(".main").append(descriptionWidthTitlePixels);
+
+		var widthInpuntPixels = document.createElement("input");
+		widthInpuntPixels.id = "widthInpuntPixels";
+		widthInpuntPixels.type = "number";
+		$(".main").append(widthInpuntPixels);
+
+		var btnPixels1366 = document.createElement("button");
+		btnPixels1366.id = "btnPixels1366";
+		btnPixels1366.innerText = "oblicz";
+		$(".main").append(btnPixels1366);
+
+		var widthPercents = document.createElement("p");
+		widthPercents.id = "widthPercents";
+		widthPercents.innerText = "wynik to: ";
+		$(".main").append(widthPercents);
+
+		var scorePercents = document.createElement("div");
+		scorePercents.id = "scorePercents";
+		scorePercents.innerText = "";
+		$(".main").append(scorePercents);
+
+		$("#btnPixels1366").on("click", function () {
+			var widthPixels = $("#widthInpuntPixels").val();
+			var onePercent = 13.66;
+			$("#scorePercents").text(widthPixels / onePercent + "%");
+		});
+
+		var descriptionWidthTitlePercents = document.createElement("p");
+		descriptionWidthTitlePercents.id = "descriptionWidthTitlePercents";
+		descriptionWidthTitlePercents.innerText = "podaj liczbę (w %)";
+		$(".main").append(descriptionWidthTitlePercents);
+
+		var widthInputPercents = document.createElement("input");
+		widthInputPercents.id = "widthInputPercents";
+		widthInputPercents.type = "number";
+		$(".main").append(widthInputPercents);
+
+		var btnPercents1366 = document.createElement("button");
+		btnPercents1366.id = "btnPercents1366";
+		btnPercents1366.innerText = "oblicz";
+		$(".main").append(btnPercents1366);
+
+		var widthPixels = document.createElement("p");
+		widthPixels.id = "widthPixels";
+		widthPixels.innerText = "wynik to: ";
+		$(".main").append(widthPixels);
+
+		var scorePixels = document.createElement("div");
+		scorePixels.id = "scorePixels";
+		scorePixels.innerText = "";
+		$(".main").append(scorePixels);
+
+		$("#btnPercents1366").on("click", function () {
+			var widthPercents = $("#widthInputPercents").val();
+			var onePercent = 13.66;
+			$("#scorePixels").text(widthPercents * onePercent + "px");
+		});
 	});
-
-	$("#convert2").on("click", function () {
-		var percent = $("#percentOnWidth").val();
-
-		var onePercent = 13.66;
-		$("#score2").text(percent / onePercent + "%");
-	});
-
-	$("#fifth").on("click", function () {});
 });
 
 /***/ })
--- js/workingFile.js
+++ js/workingFile.js
@@ -1,24 +1,84 @@
 document.addEventListener("DOMContentLoaded", () => {
 console.log("NIEWIERNE PSY RULEZ!!!!");
 
-$("#convert").on("click", ()=>{
-	let widthPercent = $("#widthOnPercent").val();
-	let onePercent = 13.66;
-	$("#score").text(onePercent*widthPercent + "px");
+
+
+
+
+//obsługa zdarzeń dla przycisku dla rozdzielczości 1366x768 pikseli
+
+$("#fifth").on("click", function(){
+	
+	let title = document.createElement("p");
+	title.id = "title";
+	title.innerText = "Przeliczanie szerokości i wysokości z pikseli na procenty, dla rozdzielczości: 1366x768";
+	$(".main").append(title);
+	
+	
+	let descriptionWidthTitlePixels = document.createElement("p");
+	descriptionWidthTitlePixels.id = "descriptionWidthTitlePixels";
+	descriptionWidthTitlePixels.innerText = "podaj liczbę (w px)";
+	$(".main").append(descriptionWidthTitlePixels);
+	
+	let widthInpuntPixels = document.createElement("input");
+	widthInpuntPixels.id = "widthInpuntPixels";
+	widthInpuntPixels.type = "number";
+	$(".main").append(widthInpuntPixels);
+	
+	let btnPixels1366 = document.createElement("button");
+	btnPixels1366.id = "btnPixels1366";
+	btnPixels1366.innerText = "oblicz";
+	$(".main").append(btnPixels1366);
+	
+	let widthPercents = document.createElement("p");
+	widthPercents.id = "widthPercents";
+	widthPercents.innerText = "wynik to: ";
+	$(".main").append(widthPercents);
+	
+	let scorePercents = document.createElement("div");
+	scorePercents.id = "scorePercents";
+	scorePercents.innerText = "";
+	$(".main").append(scorePercents);
+	
+	$("#btnPixels1366").on("click", function(){
+		let widthPixels = $("#widthInpuntPixels").val();
+		let onePercent = 13.66;
+		$("#scorePercents").text(widthPixels/onePercent + "%");
+	});
+	
+		
+	
+	let descriptionWidthTitlePercents = document.createElement("p");
+	descriptionWidthTitlePercents.id = "descriptionWidthTitlePercents";
+	descriptionWidthTitlePercents.innerText = "podaj liczbę (w %)";
+	$(".main").append(descriptionWidthTitlePercents);
+	
+	let widthInputPercents = document.createElement("input");
+	widthInputPercents.id = "widthInputPercents";
+	widthInputPercents.type = "number";
+	$(".main").append(widthInputPercents);
+	
+		let btnPercents1366 = document.createElement("button");
+	btnPercents1366.id = "btnPercents1366";
+	btnPercents1366.innerText = "oblicz";
+	$(".main").append(btnPercents1366);
+	
+	let widthPixels = document.createElement("p");
+	widthPixels.id = "widthPixels";
+	widthPixels.innerText = "wynik to: ";
+	$(".main").append(widthPixels);
+	
+	let scorePixels = document.createElement("div");
+	scorePixels.id = "scorePixels";
+	scorePixels.innerText = "";
+	$(".main").append(scorePixels);
+	
+	$("#btnPercents1366").on("click", function(){
+		let widthPercents = $("#widthInputPercents").val();
+		let onePercent = 13.66;
+		$("#scorePixels").text(widthPercents*onePercent + "px");
+	});
 });
-
-$("#convert2").on("click", ()=>{
-	let percent = $("#percentOnWidth").val();
-
-	let onePercent = 13.66;
-	$("#score2").text(percent/onePercent + "%");
-});
-
-
-
-$("#fifth").on("click", () => {
-
-});
-
+	
 
 });
